d8aef25a9fedb7c2d56aac95d2546e43d74d58a6
nemlic <nemlicmain22@gmail.com>
2024-10-16 09:35:46 +0300

    revised

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,43 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { ComposeComponent } from './compose/compose.component';
+import { InboxComponent } from './inbox/inbox.component';
+import { SentComponent } from './sent/sent.component';
+import { SpamComponent } from './spam/spam.component';
+import { TrashComponent } from './trash/trash.component';
+import { NewLabelComponent } from './new-label/new-label.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ComposeComponent,
+    InboxComponent,
+    SentComponent,
+    SpamComponent,
+    TrashComponent,
+    NewLabelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
